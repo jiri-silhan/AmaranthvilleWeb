763d2507dfa2be6fdb310d086e4673a179a6af1e
JiriS2 <jiris2@kentico.com>
2018-03-15 10:23:54 +0100

    Add web template

---
+++ Web/AmaranthvilleWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmaranthvilleWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
