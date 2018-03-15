27175f2a23fbdfd771a13a57014e08ec82356963
JiriS2 <jiris2@kentico.com>
2018-03-15 15:11:15 +0100

    Add KC SDK and LanguageClient

---
+++ Web/AmaranthvilleWeb/KenticoCloudClient.cs
@@ -0,0 +1,18 @@
+﻿using KenticoCloud.Delivery;
+
+namespace AmaranthvilleWeb
+{
+    public class KenticoCloudClient
+    {
+        public const string PROJECT_ID = "20630705-3b47-4951-b2fb-6d86b5457c13";
+
+        public IDeliveryClient LanguageDeliveryClient { get; }
+
+        public KenticoCloudClient()
+        {
+            var client = new DeliveryClient(PROJECT_ID);
+            
+            LanguageDeliveryClient = new LanguageClient(client, "en-us");
+        }
+    }
+}
