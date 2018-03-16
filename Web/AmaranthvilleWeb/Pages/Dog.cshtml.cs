a98ccbe1138968e6e90f622acf3144fd5e5c8e50
Jiří Šilhán <jiris2@kentico.com>
2018-03-16 14:40:11 +0100

    Add dog to presentation

---
+++ Web/AmaranthvilleWeb/Pages/Dog.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmaranthvilleWeb.Pages
+{
+    public class DogModel : PageModel
+    {
+        public PrezentacePsa Pes { get; set; }
+        public async Task OnGetAsync()
+        {
+            var client = new KenticoCloudClient();
+            var goofy = await client.LanguageDeliveryClient.GetItemAsync<PrezentacePsa>("goofy");
+            Pes = goofy.Item;
+        }
+    }
+}
